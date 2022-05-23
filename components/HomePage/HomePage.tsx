d1ec68681fbc4f8f2debc2ad1dd6972ddbd8ed51
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2022-05-23 21:18:05 +0200

    refactor(home-page): extract HomePage to components

---
+++ components/HomePage/HomePage.tsx
@@ -0,0 +1,69 @@
+import styled from "@emotion/styled";
+
+import { ArticleCard } from "../ArticleCard";
+import { Layout } from "../Layout";
+import {
+  pageContentMarginTop,
+  responsiveContainer,
+  responsiveContainerInlinePadding,
+} from "../../styles/layout";
+import { rem, spacing } from "../../styles/theme";
+
+export const HomePage = () => {
+  return (
+    <Layout>
+      <StyledMainContent>
+        Hey, I&apos;m Greg, welcome to my blog!
+      </StyledMainContent>
+
+      <StyledArticleCardsContainer>
+        <ArticleCard
+          readingDuration="14 minutes"
+          createdDate="2022-05-01"
+          tagNames={["blog", "architecture", "frontend"]}
+          title="Blog hosting decisions"
+          summary={
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vulputate sapien vel risus sagittis, et scelerisque ex elementum. Aliquam non porttitor justo, eu bibendum tortor. Vivamus a turpis sem."
+          }
+        />
+
+        <ArticleCard
+          readingDuration="14 minutes"
+          createdDate="2022-05-01"
+          tagNames={["blog", "architecture", "frontend"]}
+          title="Blog hosting decisions"
+          summary={
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vulputate sapien vel risus sagittis, et scelerisque ex elementum. Aliquam non porttitor justo, eu bibendum tortor. Vivamus a turpis sem."
+          }
+        />
+
+        <ArticleCard
+          readingDuration="14 minutes"
+          createdDate="2022-05-01"
+          tagNames={["blog", "architecture", "frontend"]}
+          title="Blog hosting decisions"
+          summary={
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vulputate sapien vel risus sagittis, et scelerisque ex elementum. Aliquam non porttitor justo, eu bibendum tortor. Vivamus a turpis sem."
+          }
+        />
+      </StyledArticleCardsContainer>
+    </Layout>
+  );
+};
+
+const StyledMainContent = styled("main")(
+  responsiveContainer,
+  responsiveContainerInlinePadding,
+  pageContentMarginTop
+);
+
+const StyledArticleCardsContainer = styled("div")(
+  responsiveContainer,
+  responsiveContainerInlinePadding,
+  {
+    display: "flex",
+    flexDirection: "column",
+    gap: rem(spacing(2)),
+    marginBlockStart: rem(spacing(2)),
+  }
+);
