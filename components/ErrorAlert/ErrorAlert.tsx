3616290cf0b18f4d59c8fa8b83308a958856937c
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2022-06-12 10:47:32 +0200

    feat: add ErrorAlert component

---
+++ components/ErrorAlert/ErrorAlert.tsx
@@ -0,0 +1,45 @@
+import styled from "@emotion/styled";
+import { ReactNode, useEffect } from "react";
+import {
+  responsiveContainer,
+  responsiveContainerInlinePadding,
+} from "../../styles/layout";
+import { rem, spacing } from "../../styles/theme";
+
+export const ErrorAlert = ({ children }: { children: ReactNode }) => {
+  return <div>{children}</div>;
+};
+
+export const ErrorAlertContainer = styled("div")(
+  responsiveContainer,
+  responsiveContainerInlinePadding,
+  ({ theme }) => ({
+    backgroundColor: theme.color.error.background,
+    border: `solid 1px ${theme.color.error.border}`,
+    padding: rem(spacing(2)),
+    borderRadius: rem(spacing(1)),
+  })
+);
+
+export const DevOnlyErrorDetails = ({ error }: { error: unknown }) => {
+  useEffect(() => {
+    if (process.env.NODE_ENV !== "production") {
+      console.log("Error occurred", error);
+    }
+  });
+
+  if (process.env.NODE_ENV === "production") {
+    return null;
+  }
+
+  return <SerializedError>{JSON.stringify(error, null, 2)}</SerializedError>;
+};
+
+const SerializedError = styled("pre")(({ theme }) => ({
+  backgroundColor: theme.color.background.light,
+  padding: rem(spacing(3)),
+  borderRadius: rem(spacing(1)),
+  border: `solid 1px ${theme.color.text.main}`,
+  marginBlockStart: rem(spacing(2)),
+  marginBlockEnd: 0,
+}));
