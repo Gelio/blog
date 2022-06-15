9f0f4d2fc2f9fe22c4f9ca76d7350f1d2f0146e8
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2022-06-15 20:47:56 +0200

    feat(homepage): show real posts
    Read the all-contents index to show the actual posts with correct URLs
    and metadata.

--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -1,55 +1,62 @@
 import styled from "@emotion/styled";
+import { either } from "fp-ts";
+import { pipe } from "fp-ts/function";
 
-import { ArticleCard } from "../ArticleCard";
-import { Layout } from "../Layout";
+import { readAllContentIndex } from "../../content-processing/indexes";
 import {
   pageContentMarginTop,
   responsiveContainer,
   responsiveContainerInlinePadding,
 } from "../../styles/layout";
 import { rem, spacing } from "../../styles/theme";
+import { ArticleCard } from "../ArticleCard";
+import {
+  DevOnlyErrorDetails,
+  ErrorAlert,
+  ErrorAlertContainer,
+} from "../ErrorAlert";
+import { Layout } from "../Layout";
 
-export const HomePage = () => {
+interface HomePageProps {
+  allPostsResult: Awaited<ReturnType<typeof readAllContentIndex>>;
+}
+
+export const HomePage = ({ allPostsResult }: HomePageProps) => {
   return (
     <Layout>
       <StyledMainContent>
         Hey, I&apos;m Greg, welcome to my blog!
       </StyledMainContent>
 
-      <StyledArticleCardsContainer>
-        <ArticleCard
-          readingTimeMin={14}
-          createdDate="2022-05-01"
-          tagNames={["blog", "architecture", "frontend"]}
-          title="Blog hosting decisions"
-          slug="blog-hosting-decisions"
-          summary={
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vulputate sapien vel risus sagittis, et scelerisque ex elementum. Aliquam non porttitor justo, eu bibendum tortor. Vivamus a turpis sem."
-          }
-        />
-
-        <ArticleCard
-          readingTimeMin={14}
-          createdDate="2022-05-01"
-          tagNames={["blog", "architecture", "frontend"]}
-          title="Blog hosting decisions"
-          slug="blog-hosting-decisions"
-          summary={
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vulputate sapien vel risus sagittis, et scelerisque ex elementum. Aliquam non porttitor justo, eu bibendum tortor. Vivamus a turpis sem."
-          }
-        />
-
-        <ArticleCard
-          readingTimeMin={14}
-          createdDate="2022-05-01"
-          tagNames={["blog", "architecture", "frontend"]}
-          title="Blog hosting decisions"
-          slug="blog-hosting-decisions"
-          summary={
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vulputate sapien vel risus sagittis, et scelerisque ex elementum. Aliquam non porttitor justo, eu bibendum tortor. Vivamus a turpis sem."
-          }
-        />
-      </StyledArticleCardsContainer>
+      {pipe(
+        allPostsResult,
+        either.match(
+          (error) => (
+            <ErrorAlertContainer>
+              <ErrorAlert>Could not read articles metadata.</ErrorAlert>
+              <DevOnlyErrorDetails error={error} />
+            </ErrorAlertContainer>
+          ),
+          (posts) => (
+            <StyledArticleCardsContainer>
+              {posts.map((post) => (
+                <ArticleCard
+                  key={post.contentMetadata.slug}
+                  readingTimeMin={post.contentMetadata.readingTimeMin}
+                  createdDate={post.contentMetadata.date}
+                  tagNames={post.contentMetadata.tags}
+                  title={post.contentMetadata.title}
+                  slug={post.contentMetadata.slug}
+                  summary={
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vulputate sapien vel risus sagittis, et scelerisque ex elementum. Aliquam non porttitor justo, eu bibendum tortor. Vivamus a turpis sem."
+                  }
+                />
+              ))}
+              {/* TODO: add pagination */}
+            </StyledArticleCardsContainer>
+          )
+        )
+      )}
     </Layout>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,36 @@
+import { either } from "fp-ts";
+import { GetStaticProps } from "next";
+import { ComponentProps } from "react";
 import { HomePage } from "../components/HomePage";
+import { readAllContentIndex } from "../content-processing/indexes";
 
 export default HomePage;
+
+/**
+ * Max number of posts to be shown.
+ *
+ * Exceeding this number should push me to add pagination to the index page and
+ * the topic pages.
+ */
+const maxPosts = 30;
+
+export const getStaticProps: GetStaticProps<
+  ComponentProps<typeof HomePage>
+> = async () => {
+  const allPostsResult = await readAllContentIndex();
+
+  if (
+    either.isRight(allPostsResult) &&
+    allPostsResult.right.length >= maxPosts
+  ) {
+    throw new Error(
+      `Max number of posts (${maxPosts}) exceeded. There are ${allPostsResult.right.length} posts. It is time to add pagination`
+    );
+  }
+
+  return {
+    props: {
+      allPostsResult,
+    },
+  };
+};
