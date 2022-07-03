ed4a2da617744c5bc08e8bfe137bdd9feeebc0b4
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2022-07-03 13:55:39 +0200

    chore(nvim): add a :WordCount command in MDX files

--- .nvimrc.lua
+++ .nvimrc.lua
@@ -5,5 +5,7 @@ vim.api.nvim_create_autocmd("BufEnter", {
 	group = markdown_formatting_group_id,
 	callback = function()
 		vim.bo.textwidth = 80
+
+		vim.api.nvim_buf_create_user_command(0, "WordCount", "!wc -lw %", {})
 	end,
 })
