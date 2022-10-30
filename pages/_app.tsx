06b81bc01fa994f974a93dff2d32932d437a55b4
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2022-10-30 18:57:50 +0100

    feat(rss): add automatic RSS discovery `link` tag
    Based on https://www.petefreitag.com/item/384.cfm

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { MatomoTrackingScript } from "../lib/matomo";
 import { StylesProvider } from "../styles/provider";
 
@@ -6,6 +7,15 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <StylesProvider>
       <MatomoTrackingScript />
+      <Head>
+        <link
+          key="rss-feed"
+          rel="alternate"
+          type="application/rss+xml"
+          title="RSS Feed for gregroz.me"
+          href="/rss.xml"
+        />
+      </Head>
 
       <Component {...pageProps} />
     </StylesProvider>
